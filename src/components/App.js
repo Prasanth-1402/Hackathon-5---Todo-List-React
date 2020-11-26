9b62c72e80890a4ee4758e306b0786f11f5d0b4b
Prasanth-1402 <prasanthshanmugam1402@gmail.com>
2020-11-26 15:46:14 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import "./../styles/App.css";
-
+import ListItem from "./ListItem.js";
 function App() 
 {
+	const [items, setItems] = useState([]);
+	const [newItem, setNewItem] = useState("");
+	const addItem = () => {
+		items.push(newItem);
+		setItems([...items]);
+		setNewItem("");
+		// console.log([...items]);
+	}
+
+	const newItemChanged = (evt) =>{
+		setNewItem(evt.target.value);
+	}
+	const deleteHandler = (itemIndex) => {
+		items.splice(itemIndex, 1);
+		setItems([...items]);
+	}
+	const editHandler = (editedValue, itemidx) => {
+		items[itemidx] = editedValue;
+		setItems([...items]);
+	}	
+
 	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
-	</div>
+		<>
+			<div id="main">
+				<textarea onChange={newItemChanged} id= "task" value={newItem} placeholder = "New Task" > </textarea>
+				<button id='btn' onClick={addItem} disabled = {newItem.trim().length === 0}> Add </button>
+				{
+					
+					items.map((item,idx) => (
+						// {console.log("Hi from map1 " + item + " "+idx)}
+						<ListItem item = {item} key = {`${item}_${idx}`} idx = {idx} editHandler = {editHandler} deleteHandler ={deleteHandler}/>
+						// {console.log("Hi from map2")}
+					))
+				} 
+			</div>
+		</>
 	);
 }
 
